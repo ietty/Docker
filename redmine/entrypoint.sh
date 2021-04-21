8bd263a8fac8d3503806d6d535740326f2c2e3a0
tanaka_shintaro <shintaro.tanaka@ietty.co.jp>
2021-04-21 15:41:13 +0900

    redmine-image作成

---
+++ redmine/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+source ${REDMINE_RUNTIME_ASSETS_DIR}/functions
+
+echo "--Section: entrypoint.sh"
+
+case ${1} in
+  app:init)
+    initialize_system
+    configure_redmine
+
+    version_check
+    migrate_database
+    install_plugins
+    install_themes
+
+    # pidが書かれているとプロセスがあると判断されるので消す
+    rm -f tmp/pids/server.pid
+
+    # TODO call rails_server()
+    # tty:trueが効かないのでtailコマンドを走らせる
+    bash -c "tail -f /dev/null"
+    ;;
+  app:restart)
+    version_check
+    migrate_database
+    install_plugins
+    install_themes
+    ;;
+esac
