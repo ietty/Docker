9d83d094f6fd9017f8bf35c77ba27e4d13e44708
shintarotanaka-ietty <31497000+shintarotanaka-ietty@users.noreply.github.com>
2022-06-16 15:32:52 +0900

    イメージビルドのシェルを修正

--- ubuntu/focal-ruby/build.sh
+++ ubuntu/focal-ruby/build.sh
@@ -3,5 +3,5 @@ tag=latest
 
 registry=ietty
 docker build -t ${registry}/${name}:${tag} .
-#docker login
-#docker push ${registry}/${name}:${tag}
+docker login
+docker push ${registry}/${name}:${tag}
