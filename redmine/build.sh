89b51f1c5de79cebce51c9d7b7b1ac7913a2af9b
shintarotanaka-ietty <31497000+shintarotanaka-ietty@users.noreply.github.com>
2022-08-03 22:24:19 +0900

    シェルスクリプト修正

--- redmine/build.sh
+++ redmine/build.sh
@@ -6,7 +6,6 @@ tag=$1
 
 docker build -t ${registry}/${name}:${tag} .
 docker run -it --rm ${registry}/${name} bash
-docker push ${registry}/${name}:${tag}
 
 function pushDockerHub(){
     docker tag ${registry}/${name}:${tag} ${registry}/${name}:latest
@@ -16,11 +15,9 @@ function pushDockerHub(){
 }
 
 function pushPrivateECR(){
-    aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${private_ecr}
-    aws ecr create-repository --repository-name ${name}
-    docker tag ${registry}/${name}:${tag} ${private_ecr}/${name}:${tag}
-    docker push ${private_ecr}/${name}:${tag}
-    echo "pushed: ${private_ecr}/${name}:${tag}"
+    aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 888777505088.dkr.ecr.ap-northeast-1.amazonaws.com
+    docker tag ${registry}/${name}:${tag} ${private_ecr}/redmine:latest
+    docker push ${private_ecr}/redmine:latest
 }
 
 # pushDockerHub
