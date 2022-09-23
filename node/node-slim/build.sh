8c348f1b69e630ec2c4efbab53aaff49c9b28d62
takemi-ohama <takemi.ohama@gmail.com>
2022-09-23 05:10:03 +0000

    node-slimを追加

--- node/node-slim/build.sh
+++ node/node-slim/build.sh
@@ -2,9 +2,7 @@ name=`basename ${PWD}`
 tag=`date +%Y%m%d`01
 
 registry=ietty
-public_ecr=public.ecr.aws/b5w9v1j5
 private_ecr=888777505088.dkr.ecr.ap-northeast-1.amazonaws.com
-
 
 docker build --no-cache -t ${registry}/${name}:${tag} .
 docker tag ${registry}/${name}:${tag} ${registry}/${name}:latest
@@ -15,27 +13,18 @@ function pushDockerHub(){
     docker push ${registry}/${name}:latest
     echo "pushed: ${registry}/${name}:${tag}"
 }
-
-function pushPublicECR(){
-    aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${public_ecr}
-    aws --region us-east-1 ecr-public create-repository --repository-name ${name}
-    docker tag ${registry}/${name}:${tag} ${public_ecr}/${name}:${tag}
-    docker tag ${registry}/${name}:${tag} ${public_ecr}/${name}:latest
-    docker push ${public_ecr}/${name}:${tag}
-    docker push ${public_ecr}/${name}:latest
-    echo "pushed: ${public_ecr}/${name}:${tag}"
-}
-
 function pushPrivateECR(){
     aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${private_ecr}
     aws ecr create-repository --repository-name ${name}
     docker tag ${registry}/${name}:${tag} ${private_ecr}/${name}:${tag}
-    docker tag ${registry}/${name}:${tag} ${private_ecr}/${name}:latest
     docker push ${private_ecr}/${name}:${tag}
     docker push ${private_ecr}/${name}:latest
     echo "pushed: ${private_ecr}/${name}:${tag}"
+    echo "pushed: ${private_ecr}/${name}:latest"
 }
 
 
 pushDockerHub
-pushPublicECR
+#pushPublicECR
+#pushPrivateECR
+
