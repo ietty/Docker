845160d611bcb942e35757359f9b14257ad0ea81
shintarotanaka-ietty <31497000+shintarotanaka-ietty@users.noreply.github.com>
2023-01-10 01:55:15 +0900

    create build shell

--- redmine-new/build.sh
+++ redmine-new/build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 registry=ietty
 name=redmine-new
+private_ecr=888777505088.dkr.ecr.ap-northeast-1.amazonaws.com
 tag=latest
 
-docker build -t ${registry}/${name}:${tag} .
-docker run -it --rm ${registry}/${name} bash
+docker build --no-cache -t ${registry}/${name}:${tag} .
+docker tag ${registry}/${name}:${tag} ${registry}/${name}:latest
+
+function pushPrivateECR(){
+    aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 888777505088.dkr.ecr.ap-northeast-1.amazonaws.com
+    docker tag ${registry}/${name}:${tag} ${private_ecr}/${name}:latest
+    docker push ${private_ecr}/${name}:latest
+    echo "pushed: ${private_ecr}/${name}:${tag}"
+}
+
+pushPrivateECR
