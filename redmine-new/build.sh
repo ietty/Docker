b8f986c9d315344d5e3559fab1675601a4183e9c
shintarotanaka-ietty <31497000+shintarotanaka-ietty@users.noreply.github.com>
2023-01-10 20:44:25 +0900

    ruby-buildの導入

--- redmine-new/build.sh
+++ redmine-new/build.sh
@@ -2,16 +2,16 @@
 registry=ietty
 name=redmine-new
 private_ecr=888777505088.dkr.ecr.ap-northeast-1.amazonaws.com
-tag=latest
+tag=`date +"%Y%m%d"`
 
 docker build --no-cache -t ${registry}/${name}:${tag} .
-docker tag ${registry}/${name}:${tag} ${registry}/${name}:latest
+docker tag ${registry}/${name}:${tag} ${registry}/${name}:${tag}
 
 function pushPrivateECR(){
     aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 888777505088.dkr.ecr.ap-northeast-1.amazonaws.com
-    docker tag ${registry}/${name}:${tag} ${private_ecr}/${name}:latest
-    docker push ${private_ecr}/${name}:latest
+    docker tag ${registry}/${name}:${tag} ${private_ecr}/${name}:${tag}
+    docker push ${private_ecr}/${name}:${tag}
     echo "pushed: ${private_ecr}/${name}:${tag}"
 }
 
-pushPrivateECR
+#pushPrivateECR
