7db8cc554d99abb32e132e9bd0854d719e1e41f8
shintarotanaka-ietty <31497000+shintarotanaka-ietty@users.noreply.github.com>
2023-01-17 18:52:09 +0900

    コメントアウトしたコードを削除

--- redmine-new/build.sh
+++ redmine-new/build.sh
@@ -3,7 +3,6 @@ registry=ietty
 name=ubuntu-redmine
 private_ecr=888777505088.dkr.ecr.ap-northeast-1.amazonaws.com
 tag=latest
-# tag=`date +"%Y%m%d"`
 
 docker build --no-cache -t ${registry}/${name}:${tag} .
 docker tag ${registry}/${name}:${tag} ${registry}/${name}:${tag}
