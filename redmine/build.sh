a049d1918217740313214ebb6672b75f85595168
shintarotanaka-ietty <31497000+shintarotanaka-ietty@users.noreply.github.com>
2023-01-19 16:06:10 +0900

    rename/replace

--- redmine/build.sh
+++ redmine/build.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 registry=ietty
-name=redmine
+name=ubuntu-redmine
 private_ecr=888777505088.dkr.ecr.ap-northeast-1.amazonaws.com
-tag=$1
+tag=latest
 
-docker build -t ${registry}/${name}:${tag} .
-docker run -it --rm ${registry}/${name} bash
-
-function pushDockerHub(){
-    docker tag ${registry}/${name}:${tag} ${registry}/${name}:latest
-    docker login
-    docker push ${registry}/${name}:${tag}
-    echo "pushed: ${registry}/${name}:${tag}"
-}
+docker build --no-cache -t ${registry}/${name}:${tag} .
+docker tag ${registry}/${name}:${tag} ${registry}/${name}:${tag}
 
 function pushPrivateECR(){
     aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 888777505088.dkr.ecr.ap-northeast-1.amazonaws.com
-    docker tag ${registry}/${name}:${tag} ${private_ecr}/redmine:latest
-    docker push ${private_ecr}/redmine:latest
+    docker tag ${registry}/${name}:${tag} ${private_ecr}/${name}:${tag}
+    docker push ${private_ecr}/${name}:${tag}
+    echo "pushed: ${private_ecr}/${name}:${tag}"
 }
 
-# pushDockerHub
 pushPrivateECR
